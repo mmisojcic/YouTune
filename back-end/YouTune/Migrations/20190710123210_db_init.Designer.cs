28f4cde9f143012083290c12deb45798b0a39014
mmisojcic <markomisojcic@gmail.com>
2019-07-10 14:50:41 +0200

    new db migrations

--- back-end/YouTune/Migrations/20190710123210_db_init.Designer.cs
+++ back-end/YouTune/Migrations/20190710123210_db_init.Designer.cs
@@ -10,14 +10,14 @@ using YouTune.Models;
 namespace YouTune.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20190524101507_db_init")]
+    [Migration("20190710123210_db_init")]
     partial class db_init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
+                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
